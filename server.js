dcee4bbd051bd66fa456c5762d8594fb18dedd69
KiHoon Roh <started677@gmail.com>
2022-09-16 21:10:43 +0900

    feat: heroku sleep 방지 기능 추가

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const comment = require("./comments.json");
 const jsonServer = require("json-server");
+const { wakeDyno } = require("heroku-keep-awake");
 
 const server = jsonServer.create();
 const router = jsonServer.router(comment);
@@ -7,8 +8,17 @@ const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 4000;
 
+const DYNO_URL = "https://comment-api.herokuapp.com/";
+const opts = {
+  interval: 29,
+  logging: false,
+};
+
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
 server.use(router);
 
-server.listen(port, () => console.log("Server is Running"));
+server.listen(port, () => {
+  console.log("Server is Running");
+  wakeDyno(DYNO_URL, opts);
+});
